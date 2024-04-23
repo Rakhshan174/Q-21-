e159eaff5c3018bc03a1b6a2843e1dbaf81cbce2
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 08:06:17 -0700

    Q 21 TS/JS for 45 assignment

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Think of somthing you could store in a TypeScript object. Write a program 
+// that creates objects containing these items.
+let person = { name: "Rakhshi", fname: "Female", age: 22 };
+console.log(person);
---
+++ main.ts
@@ -0,0 +1,5 @@
+// Think of somthing you could store in a TypeScript object. Write a program 
+// that creates objects containing these items.
+
+let person: {name: string, fname: string , age: number} ={name:"Rakhshi", fname: "Female", age: 22}
+console.log(person)
